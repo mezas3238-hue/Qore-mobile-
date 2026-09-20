f82c7e907575d7a4e2484db984d97a5621aa2595
mezas3238-hue <mezas3238@gmail.com>
2026-09-20 09:05:30 -0400

    test: remove unused material import

--- apps/mobile/test/dashboard_connected_test.dart
+++ apps/mobile/test/dashboard_connected_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:qore_mobile/data/qore_gateway_client.dart';
 import 'package:qore_mobile/domain/models.dart';
